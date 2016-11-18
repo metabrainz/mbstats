776f99d60badf69a2e5b4609a63ff8563b2436ff
Laurent Monin <github@norz.org>
2016-11-18 23:15:22 +0100

    Add license, cite logster

--- files/stats.parser.py
+++ files/stats.parser.py
@@ -1,3 +1,48 @@
+#!/usr/bin/python2 -tt
+# -*- coding: utf-8 -*-
+
+###
+###  stats.parser.py
+###
+###  Tails a log and applies mbstats parser, then reports metrics to InfluxDB
+###
+###  Usage:
+###
+###    $ stats.parser.py [options]
+###
+###  Help:
+###
+###    $ stats.parser.py -h
+###
+###
+###  Copyright 2016, MetaBrainz Foundation
+###  Author: Laurent Monin
+###
+###  stats.parser.py is free software: you can redistribute it and/or modify
+###  it under the terms of the GNU General Public License as published by
+###  the Free Software Foundation, either version 3 of the License, or
+###  (at your option) any later version.
+###
+###  stats.parser.py is distributed in the hope that it will be useful,
+###  but WITHOUT ANY WARRANTY; without even the implied warranty of
+###  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+###  GNU General Public License for more details.
+###
+###  You should have received a copy of the GNU General Public License
+###  along with Logster. If not, see <http://www.gnu.org/licenses/>.
+###
+###  Include bits of code from Etsy Logster
+###  https://github.com/etsy/logster
+###
+###  Logster itself was forked from the ganglia-logtailer project
+###  (http://bitbucket.org/maplebed/ganglia-logtailer):
+###    Copyright Linden Research, Inc. 2008
+###    Released under the GPL v2 or later.
+###    For a full description of the license, please visit
+###    http://www.gnu.org/licenses/gpl.txt
+###
+
+
 from collections import defaultdict
 from influxdb import InfluxDBClient
 from pygtail import Pygtail
@@ -564,7 +609,7 @@ options = parser.parse_args()
 print(options)
 filename = options.file
 if not filename:
-    parser.print_help()
+    parser.print_usage()
     sys.exit(1)
 
 if options.locker == 'portalocker':
