79ed52c99a41bd7b4c2c90b6255f3bda8d90ee6c
Laurent Monin <github@norz.org>
2016-11-18 14:25:43 +0100

    Improve test, add a sequence of small processing steps

--- files/test.sh
+++ files/test.sh
@@ -8,6 +8,8 @@ STATSLOG=~/src/mbstats.testlogs/stats.log
 COMMON_OPTS=" stats.parser.py -f $STATSLOG "
 CMD="$INTERPRETER $COMMON_OPTS"
 $CMD -m 1000 --deletedatabase --startover;
-$CMD -m 100000;
-$CMD -m 1000000;
+$CMD -m 300000;
+for i in $(seq 1 10); do
+	$CMD -m 70000;
+done
 $CMD -m 2000000;
