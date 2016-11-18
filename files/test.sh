dc67a867d7926df07f2848bd507ec695e71b9074
Laurent Monin <github@norz.org>
2016-11-18 17:00:34 +0100

    Improve test script

--- files/test.sh
+++ files/test.sh
@@ -4,9 +4,20 @@ set -xe
 INTERPRETER=$1
 [ -z "$INTERPRETER" ] && INTERPRETER="python"
 
-STATSLOG=~/src/mbstats.testlogs/stats.log
-COMMON_OPTS=" stats.parser.py -f $STATSLOG "
-CMD="$INTERPRETER $COMMON_OPTS"
+STATSLOG_SOURCE=~/src/mbstats.testlogs/stats.log
+NOW=$(date -u +'%FT%TZ')
+TESTTMPDIR=$(mktemp -d -t tmp.XXXXXXXXXX)
+function finish {
+  rm -rf "$TESTTMPDIR"
+}
+trap finish EXIT
+
+STATSLOG=$TESTTMPDIR/stats.log
+
+head -5000000 $STATSLOG_SOURCE > $STATSLOG
+
+COMMON_OPTS=" stats.parser.py -f $STATSLOG -w $TESTTMPDIR -n $NOW"
+CMD="time $INTERPRETER $COMMON_OPTS"
 $CMD -m 1000 --deletedatabase --startover;
 $CMD -m 300000;
 for i in $(seq 1 10); do
