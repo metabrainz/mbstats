8205fb1943315b372a652d9c147385b908d3eed5
Laurent Monin <github@norz.org>
2019-09-24 10:58:35 +0200

    Remove useless import

--- mbstats/tests/__init__.py
+++ mbstats/tests/__init__.py
@@ -3,7 +3,6 @@ import unittest
 
 def get_suite():
     "Return a unittest.TestSuite."
-    import mbstats.tests
     loader = unittest.TestLoader()
     suite = loader.discover('.', pattern='test_*.py')
     return suite
