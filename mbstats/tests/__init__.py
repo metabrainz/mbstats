e94524f230e941f3a4f70e7d30840edf4f75e3bf
Laurent Monin <github@norz.org>
2019-09-24 10:44:18 +0200

    Add a minimal setup.py

---
+++ mbstats/tests/__init__.py
@@ -0,0 +1,8 @@
+import unittest
+
+def get_suite():
+    "Return a unittest.TestSuite."
+    import mbstats.tests
+    loader = unittest.TestLoader()
+    suite = loader.discover('.', pattern='test_*.py')
+    return suite
