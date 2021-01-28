c2ae15eed9f2cf409d1c8b48ccdd770fdaf50797
Laurent Monin <github@norz.org>
2021-01-28 11:41:18 +0100

    More complete tests, including conversion errors

--- mbstats/tests/test_parser.py
+++ mbstats/tests/test_parser.py
@@ -10,6 +10,8 @@ from mbstats.app import (
     main,
     parse_upstreams,
     parseline,
+    ParseSkip,
+    PosField
 )
 
 
@@ -36,6 +38,14 @@ class TestParser(unittest.TestCase):
 
         self.log_numlines = count
         #print("Log has %d lines" % self.log_numlines)
+        self.sample_line = '1|1568962563.374|musicbrainz.org|s|ws|200|2799|2.5|289|0.026|10.2.2.31:65412|200|0.024|0.000|0.024'
+
+    def get_sample_line(self, n, replace_with='xxx', line=None):
+        if line is None:
+            line = self.sample_line
+        parts = line.split('|')
+        parts[n] = replace_with
+        return '|'.join(parts)
 
     def tearDown(self):
         # Close the file, the directory will be removed after the test
@@ -211,48 +221,167 @@ class TestParser(unittest.TestCase):
         self.assertEqual(last_msec, 1568962563.374)
         self.assertEqual(bucket, 1568962564)
 
-        with self.assertRaises(ValueError):
-            line = '1|x1568962563.374|musicbrainz.org|s|ws|200|2799|2.5|289|0.026|10.2.2.31:65412|200|0.024|0.000|0.024'
+    def test_parseline_version_invalid(self):
+        line = self.get_sample_line(PosField.version)
+        with self.assertRaisesRegex(ParseSkip, "^invalid log version: xxx$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|x200|2799|2.5|289|0.026|10.2.2.31:65412|200|0.024|0.000|0.024'
+    def test_parseline_msec_valid(self):
+        row, last_msec, bucket = parseline(self.sample_line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(last_msec, 1568962563.374)
+
+    def test_parseline_msec_invalid(self):
+        line = self.get_sample_line(PosField.msec)
+        with self.assertRaisesRegex(ParseSkip, "^could not convert string to float: 'xxx'"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|200|x2799|2.5|289|0.026|10.2.2.31:65412|200|0.024|0.000|0.024'
+    def test_parseline_vhost(self):
+        line = self.get_sample_line(PosField.vhost, replace_with='xxx')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['vhost'], 'xxx')
+
+    def test_parseline_protocol(self):
+        line = self.get_sample_line(PosField.protocol, replace_with='xxx')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['protocol'], 'xxx')
+
+    def test_parseline_loctag(self):
+        line = self.get_sample_line(PosField.loctag, replace_with='xxx')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['loctag'], 'xxx')
+
+    def test_parseline_status_invalid(self):
+        line = self.get_sample_line(PosField.status, replace_with='xxx')
+        with self.assertRaisesRegex(ParseSkip, "^invalid literal for int\(\) with base 10: 'xxx'$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|200|2799|x2.5|289|0.026|10.2.2.31:65412|200|0.024|0.000|0.024'
+    def test_parseline_status_valid(self):
+        line = self.get_sample_line(PosField.status, replace_with='444')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['status'], 444)
+
+    def test_parseline_bytes_sent_invalid(self):
+        line = self.get_sample_line(PosField.bytes_sent, replace_with='xxx')
+        with self.assertRaisesRegex(ParseSkip, "^invalid literal for int\(\) with base 10: 'xxx'$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|200|2799|2.5|x289|0.026|10.2.2.31:65412|200|0.024|0.000|0.024'
+    def test_parseline_bytes_sent_valid(self):
+        line = self.get_sample_line(PosField.bytes_sent, replace_with='12345')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['bytes_sent'], 12345)
+
+    def test_parseline_gzip_ratio_invalid(self):
+        line = self.get_sample_line(PosField.gzip_ratio, replace_with='xxx')
+        with self.assertRaisesRegex(ParseSkip, "^could not convert string to float: 'xxx'$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|200|2799|2.5|289|x0.026|10.2.2.31:65412|200|0.024|0.000|0.024'
+    def test_parseline_gzip_ratio_valid(self):
+        line = self.get_sample_line(PosField.gzip_ratio, replace_with='12.34')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['gzip_ratio'], 12.34)
+
+    def test_parseline_request_length_invalid(self):
+        line = self.get_sample_line(PosField.request_length, replace_with='xxx')
+        with self.assertRaisesRegex(ParseSkip, "^invalid literal for int\(\) with base 10: 'xxx'$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|200|2799|2.5|289|0.026|10.2.2.31:65412|200|x0.024|0.000|0.024'
+    def test_parseline_request_length_valid(self):
+        line = self.get_sample_line(PosField.request_length, replace_with='12345')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['request_length'], 12345)
+
+    def test_parseline_request_time_invalid(self):
+        line = self.get_sample_line(PosField.request_time, replace_with='xxx')
+        with self.assertRaisesRegex(ParseSkip, "^could not convert string to float: 'xxx'$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|200|2799|2.5|289|0.026|10.2.2.31:65412|200|0.024|x0.000|0.024'
+    def test_parseline_request_time_valid(self):
+        line = self.get_sample_line(PosField.request_time, replace_with='12.34')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['request_time'], 12.34)
+
+    def test_parseline_upstream_status_invalid(self):
+        line = self.get_sample_line(PosField.upstream_status, replace_with='xxx')
+        with self.assertRaisesRegex(ParseSkip, "^invalid literal for int\(\) with base 10: 'xxx'$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|200|2799|2.5|289|0.026|10.2.2.31:65412|200|0.024|0.000|x0.024'
+    def test_parseline_upstream_status_valid(self):
+        line = self.get_sample_line(PosField.upstream_status, replace_with='444')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['upstreams']['status']['10.2.2.31:65412'], {'444': 1})
+
+    def test_parseline_upstream_addr_invalid(self):
+        line = self.get_sample_line(PosField.upstream_addr, replace_with='-')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertNotIn('upstreams', row)
+
+    def test_parseline_upstream_addr_valid_simple(self):
+        line = self.get_sample_line(PosField.upstream_addr, replace_with='6.6.6.6')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertIn('upstreams', row)
+        self.assertIn('6.6.6.6', row['upstreams']['status'])
+        self.assertIn('6.6.6.6', row['upstreams']['response_time'])
+        self.assertIn('6.6.6.6', row['upstreams']['connect_time'])
+        self.assertIn('6.6.6.6', row['upstreams']['header_time'])
+        self.assertEqual(row['upstreams']['servers_contacted'], 1)
+        self.assertEqual(row['upstreams']['internal_redirects'], 0)
+        self.assertIn('6.6.6.6', row['upstreams']['servers'])
+
+    def test_parseline_upstream_addr_valid_one_redirect(self):
+        line = self.get_sample_line(PosField.upstream_addr, replace_with='6.6.6.6 : 7.7.7.7')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertIn('upstreams', row)
+        self.assertIn('6.6.6.6', row['upstreams']['status'])
+        self.assertIn('6.6.6.6', row['upstreams']['response_time'])
+        self.assertIn('6.6.6.6', row['upstreams']['connect_time'])
+        self.assertIn('6.6.6.6', row['upstreams']['header_time'])
+        self.assertEqual(row['upstreams']['servers_contacted'], 1)
+        self.assertEqual(row['upstreams']['internal_redirects'], 1)
+        self.assertIn('6.6.6.6', row['upstreams']['servers'])
+
+    def test_parseline_upstream_addr_valid_multiple(self):
+        line = "1|1611817838.597|musicbrainz.org|-|ws|200|6110|-|516|0.726|10.2.2.36:65412 : 195.201.47.148:80|200 : 200|0.012 : 0.712|0.000 : 0.000|0.012 : 0.712"
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertIn('upstreams', row)
+        for addr in ('10.2.2.36:65412', '195.201.47.148:80'):
+            self.assertIn(addr, row['upstreams']['response_time'])
+            self.assertIn(addr, row['upstreams']['connect_time'])
+            self.assertIn(addr, row['upstreams']['header_time'])
+            self.assertIn(addr, row['upstreams']['servers'])
+            self.assertIn(addr, row['upstreams']['status'])
+            self.assertEqual(row['upstreams']['status'][addr], {'200': 1})
+        self.assertEqual(row['upstreams']['servers_contacted'], 1)
+        self.assertEqual(row['upstreams']['internal_redirects'], 1)
+
+    def test_parseline_upstream_response_time_invalid(self):
+        line = self.get_sample_line(PosField.upstream_response_time, replace_with='xxx')
+        with self.assertRaisesRegex(ParseSkip, "^could not convert string to float: 'xxx'$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|200x|2799|2.5|289|0.026|10.2.2.31:65412|200|0.024|0.000|0.024'
+    def test_parseline_upstream_response_time_valid(self):
+        line = self.get_sample_line(PosField.upstream_response_time, replace_with='12.34')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['upstreams']['response_time']['10.2.2.31:65412'], 12.34)
+
+    def test_parseline_upstream_connect_time_invalid(self):
+        line = self.get_sample_line(PosField.upstream_connect_time, replace_with='xxx')
+        with self.assertRaisesRegex(ParseSkip, "^could not convert string to float: 'xxx'$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
 
-        with self.assertRaises(ValueError):
-            line = '1|1568962563.374|musicbrainz.org|s|ws|200|2799|2.5|289|0.026|10.2.2.31:65412|200x|0.024|0.000|0.024'
+    def test_parseline_upstream_connect_time_valid(self):
+        line = self.get_sample_line(PosField.upstream_connect_time, replace_with='12.34')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['upstreams']['connect_time']['10.2.2.31:65412'], 12.34)
+
+    def test_parseline_upstream_header_time_invalid(self):
+        line = self.get_sample_line(PosField.upstream_header_time, replace_with='xxx')
+        with self.assertRaisesRegex(ParseSkip, "^could not convert string to float: 'xxx'$"):
             row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+
+    def test_parseline_upstream_header_time_valid(self):
+        line = self.get_sample_line(PosField.upstream_header_time, replace_with='12.34')
+        row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
+        self.assertEqual(row['upstreams']['header_time']['10.2.2.31:65412'], 12.34)
+
 if __name__ == '__main__':
     unittest.main()
