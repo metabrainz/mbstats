bfc1d8df33ba74fd59ccadfa76faaf3eee125825
Laurent Monin <github@norz.org>
2021-01-29 11:18:51 +0100

    Add additionnal tests for process_bucket() and mbspostprocess()

--- mbstats/tests/test_parser.py
+++ mbstats/tests/test_parser.py
@@ -9,10 +9,16 @@ import unittest
 from mbstats.app import (
     ParseSkip,
     PosField,
+    get_default_status,
+    get_storage,
     main,
+    mbsdict,
+    mbspostprocess,
     parse_upstreams,
     parseline,
+    process_bucket,
 )
+from mbstats.utils import bucket2time
 
 
 LINES_TO_PARSE = 10
@@ -405,6 +411,113 @@ class TestParser(unittest.TestCase):
         line = self.get_sample_line(PosField.upstream_header_time, replace_with='12.34')
         row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=1, last_msec=0)
         self.assertEqual(row['upstreams']['header_time']['10.2.2.31:65412'], 12.34)
+
+    def test_parser_all(self):
+        storage = get_storage()
+        mbs = mbsdict()
+        bucket_duration = 5
+        status = get_default_status(bucket_duration, 0, 30)
+        sample_line = '1|1568962563.374|musicbrainz.org|s|ws|200|2799|2.5|289|0.026|10.2.2.31:65412|200|0.027|0.057|0.024'
+        start = 1568962563.374
+
+        for i in range(20):
+            line = self.get_sample_line(PosField.msec, replace_with=str(start), line=sample_line)
+            if i == 3:
+                # just to ensure upstream header time absence gives correct result
+                line = self.get_sample_line(PosField.upstream_header_time, replace_with='-', line=line)
+            elif i == 6:
+                line = self.get_sample_line(PosField.upstream_header_time, replace_with='x', line=line)
+            if i % 2:
+                line = self.get_sample_line(PosField.status, replace_with='302', line=line)
+            try:
+                row, last_msec, bucket = parseline(line, ignore_before=0, bucket_duration=bucket_duration, last_msec=0)
+                storage[bucket].append(row)
+                self.assertEqual(last_msec, start)
+            except ParseSkip:
+                pass
+            start += bucket_duration / 3.0
+
+        buckets = list(storage)
+        self.assertEqual(buckets, [313792513, 313792514, 313792515, 313792516, 313792517, 313792518, 313792519,
+            313792520])
+        self.assertEqual(list(map(len, storage.values())), [1, 3, 2, 3, 3, 3, 3, 1])
+        self.assertEqual(list(map(lambda v: bucket2time(v, bucket_duration), storage)), ['2019-09-20T06:56:05Z', '2019-09-20T06:56:10Z', '2019-09-20T06:56:15Z', '2019-09-20T06:56:20Z', '2019-09-20T06:56:25Z', '2019-09-20T06:56:30Z', '2019-09-20T06:56:35Z', '2019-09-20T06:56:40Z'])
+
+        # process first bucket
+        current = buckets[0]
+        key = (current, 'musicbrainz.org', 's', 'ws')
+        self.assertNotIn(key, mbs['bytes_sent'])
+
+        process_bucket(current, storage, status, mbs)
+        self.assertEqual(len(storage[current]), 0)
+        self.assertIn(key, mbs['bytes_sent'])
+        self.assertEqual(mbs['bytes_sent'][key], 2799)
+
+        # process second bucket
+        current = buckets[1]
+        key = (current, 'musicbrainz.org', 's', 'ws')
+        self.assertNotIn(key, mbs['bytes_sent'])
+
+        process_bucket(current, storage, status, mbs)
+        self.assertEqual(len(storage[current]), 0)
+        self.assertIn(key, mbs['bytes_sent'])
+        self.assertEqual(mbs['bytes_sent'][key], 2799*3)
+
+        upstream_key = (current, 'musicbrainz.org', 's', 'ws', '10.2.2.31:65412')
+        # NOTE: for this bucket, we actually made a variation for upstream header time (si if i == 3 above)
+        # NOTE: float imprecision can lead to a result not exactly equal to expected value
+        self.assertAlmostEqual(mbs['_upstreams_header_time_premean'][upstream_key], 0.0480, places=4)
+        self.assertEqual(mbs['_upstreams_header_time_count_premean'][upstream_key], 2)
+
+        # process third bucket
+        current = buckets[2]
+        key = (current, 'musicbrainz.org', 's', 'ws')
+        self.assertNotIn(key, mbs['bytes_sent'])
+
+        process_bucket(current, storage, status, mbs)
+        self.assertEqual(len(storage[current]), 0)
+        self.assertIn(key, mbs['bytes_sent'])
+        # NOTE: one was skipped due to forced parse error (see if i == 6 above)
+        self.assertEqual(mbs['bytes_sent'][key], 2799*2)
+
+        upstream_key = (current, 'musicbrainz.org', 's', 'ws', '10.2.2.31:65412')
+        # NOTE: float imprecision can lead to a result not exactly equal to expected value
+        self.assertAlmostEqual(mbs['_upstreams_header_time_premean'][upstream_key], 0.0480, places=4)
+        self.assertEqual(mbs['_upstreams_header_time_count_premean'][upstream_key], 2)
+
+        # process fourth bucket
+        current = buckets[3]
+        key = (current, 'musicbrainz.org', 's', 'ws')
+        self.assertNotIn(key, mbs['bytes_sent'])
+
+        process_bucket(current, storage, status, mbs)
+        self.assertEqual(len(storage[current]), 0)
+        self.assertIn(key, mbs['bytes_sent'])
+        self.assertEqual(mbs['bytes_sent'][key], 2799*3)
+
+        upstream_key = (current, 'musicbrainz.org', 's', 'ws', '10.2.2.31:65412')
+        # NOTE: float imprecision can lead to a result not exactly equal to expected value
+        self.assertAlmostEqual(mbs['_upstreams_header_time_premean'][upstream_key], 0.0720, places=4)
+        self.assertEqual(mbs['_upstreams_header_time_count_premean'][upstream_key], 3)
+
+        # now process all buckets
+        for bucket in buckets:
+            process_bucket(bucket, storage, status, mbs)
+
+        mbspostprocess(mbs)
+
+        count_200 = 0
+        count_302 = 0
+        for bucket in buckets:
+            key = (bucket, 'musicbrainz.org', 's', 'ws', 200)
+            count_200 += mbs['status'][key]
+            key = (bucket, 'musicbrainz.org', 's', 'ws', 302)
+            count_302 += mbs['status'][key]
+        # 20 lines, one was skipped due to forced parse error, we expect 19 total
+        self.assertEqual(count_200, 9)
+        self.assertEqual(count_302, 10)
+
+
 
 if __name__ == '__main__':
     unittest.main()
